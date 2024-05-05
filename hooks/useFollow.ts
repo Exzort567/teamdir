639587d2fb00651df8efc66e3fe5e005da497e0d
Kenneth <quibelkenneth@gmail.com>
2024-05-05 15:23:10 +0800

    Fix the bug on the following backend

--- hooks/useFollow.ts
+++ hooks/useFollow.ts
@@ -1,56 +1,58 @@
+import axios from "axios";
 import { useCallback, useMemo } from "react";
-import useCurrentUser from "./useCurrentUser"
+import { toast } from "react-hot-toast";
+
+import useCurrentUser from "./useCurrentUser";
 import useLoginModal from "./useLoginModal";
 import useUser from "./useUser";
-import toast from "react-hot-toast";
-import axios from "axios";
-
 
 const useFollow = (userId: string) => {
-    const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
-    const { mutate: mutateFetchedUser } = useUser(userId);
+  const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
+  const { mutate: mutateFetchedUser } = useUser(userId);
 
-    const loginModal = useLoginModal();
+  const loginModal = useLoginModal();
 
-    const isFollowing = useMemo(() => {
-        const list = currentUser?.followingIds || [];
+  const isFollowing = useMemo(() => {
+    const list = currentUser?.followingIds || [];
 
-        return list.includes(userId)
-    }, [userId, currentUser?.followingIds]);
+    return list.includes(userId);
+  }, [currentUser, userId]);
 
-    const toggleFollow = useCallback(async() => {
-        if (!currentUser) {
-            return loginModal.onOpen();
-        }
-
-        try {
-
-            let request;
-
-            if (isFollowing) {
-                request = () => axios.delete('/api/follow', { data: { userId }});
-            } else {
-                request = () => axios.post('/api/follow', { userId });
-            }
-
-
-            await request();
-
-            mutateCurrentUser();
-            mutateFetchedUser();
-
-            toast.success('Success')
-            
-        } catch (error) {
-            console.log(error)
-            toast.error('Something went wrong')
-        }
-    }, [currentUser, isFollowing, userId, mutateCurrentUser, mutateFetchedUser, loginModal]);
-
-    return {
-        isFollowing,
-        toggleFollow
+  const toggleFollow = useCallback(async () => {
+    if (!currentUser) {
+      return loginModal.onOpen();
     }
-}
+
+    try {
+      let request;
+
+      if (isFollowing) {
+        request = () => axios.delete("/api/follow", { data: { userId } });
+      } else {
+        request = () => axios.post("/api/follow", { userId });
+      }
+
+      await request();
+      mutateCurrentUser();
+      mutateFetchedUser();
+
+      toast.success("Success");
+    } catch (error) { 
+      toast.error("Something went wrong");
+    }
+  }, [
+    currentUser,
+    isFollowing,
+    userId,
+    mutateCurrentUser,
+    mutateFetchedUser,
+    loginModal,
+  ]);
+
+  return {
+    isFollowing,
+    toggleFollow,
+  };
+};
 
 export default useFollow;
--- pages/api/follow.ts
+++ pages/api/follow.ts
@@ -1,59 +1,59 @@
-import serverAuth from "@/libs/serverAuth";
 import { NextApiRequest, NextApiResponse } from "next";
-import prisma from '@/libs/prismadb'
 
-export default async function handler (
-    req: NextApiRequest,
-    res: NextApiResponse
+import prisma from "@/libs/prismadb";
+import serverAuth from "@/libs/serverAuth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-    if (req.method !== 'POST' && req.method !== 'DELETE') {
-        return res.status(405).end();
+  if (req.method !== "POST" && req.method !== "DELETE") {
+    return res.status(405).end();
+  }
+
+  try {
+    const { userId } = req.body;
+
+    const { currentUser } = await serverAuth(req, res);
+
+    if (!userId || typeof userId !== "string") {
+      throw new Error("Invalid ID");
     }
-    
-    try {
 
-        const { userId } =  req.body;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
 
-        const { currentUser } = await serverAuth(req, res);
-
-        if (!userId || userId !== 'string') {
-            throw new Error("Invalid ID")
-        }
-
-        const user = await prisma.user.findUnique({
-            where: {
-                id: userId
-            }
-        });
-
-        if (!user) {
-            throw new Error('Invalid ID')
-        }
-
-        let updatedFollowingIds = [...(user.followingIds || [])];
-
-        if (req.method === 'POST') {
-            updatedFollowingIds.push(userId);
-        }
-
-        if (req.method === 'DELETE') {
-            updatedFollowingIds = updatedFollowingIds.filter(followingId => followingId !== userId);
-        }
-
-        const updatedUser = await prisma.user.update({
-            where: {
-                id: currentUser.id
-            },
-            data: {
-                followingIds: updatedFollowingIds
-            }
-        });
-
-        return res.status(200).json(updatedUser);
-
-        
-    } catch (error) {
-        console.log(error);
-        return res.status(400).end();
+    if (!user) {
+      throw new Error("Invalid ID");
     }
+
+    let updatedFollowingIds = [...(user.followingIds || [])];
+
+    if (req.method === "POST") {
+      updatedFollowingIds.push(userId);
+    }
+
+    if (req.method === "DELETE") {
+      updatedFollowingIds = updatedFollowingIds.filter(
+        (followingId) => followingId !== userId,
+      );
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        followingIds: updatedFollowingIds,
+      },
+    });
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
 }
