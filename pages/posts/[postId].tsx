7b3eddbb3b28afa745fa8491751d77f3107c8581
Kenneth <quibelkenneth@gmail.com>
2024-05-05 15:42:42 +0800

    Backend for viewing the post

---
+++ pages/posts/[postId].tsx
@@ -0,0 +1,35 @@
+import Form from "@/components/Form";
+import Header from "@/components/Header";
+import PostItem from "@/components/posts/PostItem";
+import usePost from "@/hooks/usePost";
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+const PostView = () => {
+    const router = useRouter();
+    const { postId } = router.query;
+
+    const { data: fetchedPost, isLoading } = usePost(postId as string);
+
+    if (isLoading || !fetchedPost) {
+        return (
+            <div className="flex justify-center items-center h-full">
+                <ClipLoader color="light-blue" size={80}/>
+            </div>
+        )
+    }
+    return (        
+        <>
+            <Header label={"Teamdir"} showBackArrow/>
+
+            <PostItem data={fetchedPost}/>
+            <Form 
+                postId={postId as string}
+                isComment
+                placeholder="Post your reply"
+            />
+        </>
+    )
+}
+
+export default PostView;
